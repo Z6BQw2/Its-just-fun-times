a0e46b755ea855abf04d27e4288310e24d411b82
Tommy Morales <136229237+Z6BQw2@users.noreply.github.com>
2025-01-16 18:39:37 +0100

    Update Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -6,10 +6,31 @@ Created on Thu Aug 16 14:32:22 2024
 """
 
 import pandas as pd
+import torch
+from transformers import BertTokenizer, BertModel
 
 df = pd.read_excel('coursera_courses2.xlsx')
 df = df[df['Institution'].str[1].str.isdigit()]
 df['Institution'] = df['Institution'].apply(lambda x: x[1:4])
 
-df.to_excel('coursera_courses4_fixed.xlsx', index=False)
-print("Les données ont été sauvegardées avec succès dans 'coursera_courses4_fixed.xlsx'.")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased")
+
+def get_bert_embedding(text):
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+    cls_embedding = outputs.last_hidden_state[:, 0, :].squeeze().numpy()
+    
+    return cls_embedding
+
+df['Course Name Vectorized'] = df['Course Name'].apply(get_bert_embedding)
+
+print(df)
+
+
+
+df.to_excel('coursera_courses4_cleaned.xlsx', index=False)
+print("Les données ont été sauvegardées avec succès dans 'coursera_courses4_cleaned.xlsx'.")
+
