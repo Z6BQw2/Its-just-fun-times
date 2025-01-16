c3204258bdc9fab826adf866525a86d095f4c53f
Tommy Morales <136229237+Z6BQw2@users.noreply.github.com>
2025-01-16 18:23:52 +0100

    Update Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 14:32:22 2024
 
+@author: Tommy
+"""
+
+import pandas as pd
+
+df = pd.read_excel('coursera_courses2.xlsx')
+df = df[df['Institution'].str[1].str.isdigit()]
+df['Institution'] = df['Institution'].apply(lambda x: x[1:4])
+
+df.to_excel('coursera_courses4_fixed.xlsx', index=False)
+print("Les données ont été sauvegardées avec succès dans 'coursera_courses4_fixed.xlsx'.")
