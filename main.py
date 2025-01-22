bc5d51d40ea80057a032d8ad21cb533bf54a3cce
Tommy Morales <136229237+Z6BQw2@users.noreply.github.com>
2025-01-22 22:54:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Thu Aug 15 12:25:17 2024
 
 import pandas as pd
 import torch
+import numpy as np
 from transformers import BertTokenizer, BertModel
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier
@@ -16,10 +17,16 @@ from Data_Cleaning import get_bert_embedding
 
 df = pd.read_excel('coursera_courses4_cleaned.xlsx')
 
+def convert_to_vector(vector_string):
+    vector = np.array(vector_string)
+    return vector
 
-#Je sais pas si ça sert à quelque chose, y'a surrement des infos viable même si, si j'avais plus d'un demi-cerveau, j'aurais utilisé un truc plus cohérent que du clustering (Transfo ou que sais-je...). Mais bon, FLEMME. Je sais pas comment j'en suis venu à penser que stemming et clustering feraient bon ménage, mais YOLO.
-kmeans = KMeans(n_clusters=10, random_state=42) #Chais pas, 10 ça semble pas mal vu que Coursera est pas très diversifié (très très IA, etc surtout) et que mon PC est trop vieux pour ces conneries.
-clusters = kmeans.fit_predict(list(df['Course Name Vectorized']))
+df['Course Name Vectorized'] = df['Course Name Vectorized'].apply(convert_to_vector)
+
+vector_matrix = np.stack(df['Course Name Vectorized'])
+
+kmeans = KMeans(n_clusters=10) #Chais pas, 10 ça semble pas mal vu que Coursera est pas très diversifié (très très IA, etc surtout) et que mon PC est trop vieux pour ces conneries.
+clusters = kmeans.fit_predict(vector_matrix)
 
 df['Cluster'] = clusters
 
@@ -28,14 +35,14 @@ y = df['Course Name']
 
 X = pd.get_dummies(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-decision_tree = DecisionTreeClassifier(random_state=42)
+decision_tree = DecisionTreeClassifier()
 decision_tree.fit(X_train, y_train)
 
 y_pred = decision_tree.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Précision de l'arbre de décision : {accuracy * 100:.2f}%")
+print("Précision de l'arbre : ", accuracy * 100)
 
 def recommend_course(user_input):
     user_cluster = kmeans.predict([get_bert_embedding(user_input)])
